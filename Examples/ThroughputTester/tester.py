3f7c801d96f0df9d880b44a9c0107b333084e2ff
William Belcher <william.belcher@hotmail.com>
2023-06-12 15:09:30 +1000

    Modify throuhput tester to respect new json structure

--- Examples/ThroughputTester/tester.py
+++ Examples/ThroughputTester/tester.py
@@ -1,108 +1,84 @@
 import sys
 import time
+import uuid
 import random
 import threading
 import subprocess
+from datetime import datetime
+
 
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
 'requests'])
 
 import requests
 
-MAX_THREADS = 500
+MAX_THREADS = 100
 USLEEP = lambda x: time.sleep(x/1000000.0)
 BASE_URL = "http://localhost:8000"
-IDS = [None] * MAX_THREADS
 
 def thread_function(idx, result):  
     # Sleep between 0 and 10 us.
     USLEEP(random.randint(0, 10))
-    if IDS[idx] != None:
-        id_data = {
+
+    test_duration = random.randint(300,600)
+    start_time = datetime.now()
+    delta = 0
+    while(delta < test_duration):
+        stats_json = {
+            "id": str(uuid.uuid4()),
             "metadata": {
-                "id": IDS[idx]
+            },
+            "metrics": {
+                "mean_fps": {
+                    "description": "The average fps",
+                    "value": random.randint(0, 60)
+                },
+                "mean_frametime": {
+                    "description": "The average frame time",
+                    "value": random.randint(0, 60)
+                },
+                "mean_gamethreadtime": {
+                    "description": "The average game thread time",
+                    "value": random.randint(0, 60)
+                },
+                "mean_gputime": {
+                    "description": "The average gpu time",
+                    "value": random.randint(0, 60)
+                },
+                "mean_rendertime": {
+                    "description": "The average render thread time",
+                    "value": random.randint(0, 60)
+                },
+                "mean_rhithreadtime": {
+                    "description": "The average rhi thread time",
+                    "value": random.randint(0, 60)
+                },
+                "num_hangs": {
+                    "description": "The average number of hangs over the recorded interval",
+                    "value": random.randint(0, 60)
+                },
+                "memory_virtual": {
+                    "description": "The average virtual memory usage",
+                    "value": random.randint(0, 60)
+                },
+                "memory_physical": {
+                    "description": "The average physical memory usage",
+                    "value": random.randint(0, 60)
+                },
+                "memory_gpu": {
+                    "description": "The average gpu memory usage",
+                    "value": random.randint(0, 60)
+                }
             }
         }
 
-    res = requests.post(url=BASE_URL + "/setup", json=(id_data if IDS[idx] != None else {}))
-    if res.status_code != requests.codes.created:
-        result[idx] = False
-        return
-    res_data = res.json()
-    IDS[idx] = res_data["id"]
-    
-    setup_json = {
-        "metadata": {
-            "id": res_data["id"]
-        },
-        "metrics": {
-            "mean_fps": {
-                "description": "The average fps",
-                "type": "gauge"
-            },
-            "mean_frametime": {
-                "description": "The average fps",
-                "type": "gauge"
-            },
-            "mean_gamethreadtime": {
-                "description": "The average fps",
-                "type": "gauge"
-            },
-            "mean_gputime": {
-                "description": "The average fps",
-                "type": "gauge"
-            },
-            "mean_rendertime": {
-                "description": "The average fps",
-                "type": "gauge"
-            },
-            "mean_rhithreadtime": {
-                "description": "The average fps",
-                "type": "gauge"
-            },
-            "num_hangs": {
-                "description": "The average fps",
-                "type": "gauge"
-            },
-            "memory_virtual": {
-                "description": "The average fps",
-                "type": "gauge"
-            },
-            "memory_physical": {
-                "description": "The average fps",
-                "type": "gauge"
-            },
-            "memory_gpu": {
-                "description": "The average fps",
-                "type": "gauge"
-            }
-        }
-    }
-
-    res = requests.post(url=BASE_URL + "/setup", json=setup_json)
-    if res.status_code != requests.codes.created:
-        result[idx] = False
-        return
-    
-    for i in range(25):
-        metric_json = {
-            "id": res_data["id"],
-            "mean_fps": i,
-            "mean_frametime": i,
-            "mean_gamethreadtime": i,
-            "mean_gputime": i,
-            "mean_rendertime": i,
-            "mean_rhithreadtime": i,
-            "num_hangs": i,
-            "memory_virtual": i,
-            "memory_physical": i,
-            "memory_gpu": i
-        }
-        res = requests.post(url=BASE_URL + "/stats", json=metric_json)
+        res = requests.post(url=BASE_URL + "/stats", json=stats_json)
         if res.status_code != requests.codes.ok:
             result[idx] = False
             return
+        
         USLEEP(1000000)
+        delta = (datetime.now() - start_time).total_seconds() / 60.0
 
     result[idx] = True
 
@@ -118,15 +94,12 @@ def main():
             results.append(None)
             x.start()
 
-        for index, thread in enumerate(threads):
-            thread.join()
+    for _, thread in enumerate(threads):
+        thread.join()
 
-        if all(results):
-            print("Success")
-        else:
-            print("Fail")
-            return
-    
-
+    if all(results):
+        print("Success")
+    else:
+        print("Fail")
 
 main()
